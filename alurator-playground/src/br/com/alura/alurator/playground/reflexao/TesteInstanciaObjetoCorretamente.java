7a191755bf4491a9deb4fb33c663385ec3c8429d
Paulo Afonso Batista Gonçalves <paulo.abg@hotmail.com>
2021-06-30 20:30:13 -0300

    implementacao constructor

--- alurator-playground/src/br/com/alura/alurator/playground/reflexao/TesteInstanciaObjetoCorretamente.java
+++ alurator-playground/src/br/com/alura/alurator/playground/reflexao/TesteInstanciaObjetoCorretamente.java
@@ -1,17 +1,39 @@
 package br.com.alura.alurator.playground.reflexao;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 import br.com.alura.alurator.playground.controle.SubControle;
 
 public class TesteInstanciaObjetoCorretamente {
 
-	public static void main(String[] args) throws ClassNotFoundException {
+	public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		Class<SubControle> subControleClasse1  =  SubControle.class;
 		
 		Class<?> subControleClasse2
-			= Class.forName("br.com.alura.playground.controle.SubControle");
+			= Class.forName("br.com.alura.alurator.playground.controle.SubControle");
 		
 		Class<?> controleClasse1
-		= Class.forName("br.com.alura.playground.controle.Controle");
+		= Class.forName("br.com.alura.alurator.playground.controle.Controle");
+		
+		Constructor<SubControle> construtorSubControle1 =
+				subControleClasse1.getConstructor();
+		
+		Constructor<SubControle> construtorSubControle2 =
+				subControleClasse1.getDeclaredConstructor(String.class);
+		
+		construtorSubControle2.setAccessible(true);
+		
+		System.out.println(construtorSubControle1);
+		System.out.println(construtorSubControle2);
+		
+		Object subControle1 = construtorSubControle1.newInstance();
+		System.out.println(subControle1);
+		
+		Object subControle2 = construtorSubControle2.newInstance("");
+		System.out.println(subControle2);
+		
+		
 	}
 
 }
